7dea4d44edabb93aa760535daafe3cb0788ba0ec
EmeAsm <emerick@live.be>
2017-09-19 12:13:24 +0200

    Completed adding user to Firebase

--- Party Face/Controller/SignInVC.swift
+++ Party Face/Controller/SignInVC.swift
@@ -57,7 +57,8 @@ class SignInVC: UIViewController, UITextFieldDelegate {
                 if error == nil {
                     print("EMERICK: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -68,7 +69,8 @@ class SignInVC: UIViewController, UITextFieldDelegate {
                             print("EMERICK: Successfully authenticated with Firebase")
                             self.passwordError.isHidden = true
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData:  userData)
                             }
                         }
                     })
@@ -78,7 +80,9 @@ class SignInVC: UIViewController, UITextFieldDelegate {
         
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
+        
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("EMERICK: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
